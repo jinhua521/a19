bcbe0b3c9669650b87ffb7588e8c21ba06da9849
shadiao <1498800286@qq.com>
2019-01-17 15:33:03 +0800

    显示所有敏感字符

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
 int main(){
+    char arr[1024];
+    int index =0;
+    int isMingan(char x){
+        int count = 0;
+        for(int i=0; i<index;i++){
+            if(x == arr[i]){
+                count++;
+            }
+            }
+             if(count == 0){
+                return 0;
+            }else{
+                return 1;
+        }
+    }
 
    printf("jinrumingganzifuguanlixitong\n");
      while(1){
@@ -19,7 +34,7 @@ int main(){
               scanf("%c",&m);
               scanf("%c",&m);
              
-              printf("tianjiachenggong\n");
+              printf("tianjiachenggong,dianjihuichejixu\n");
               char x;
               scanf("%c",&x);
               scanf("%c",&x);
@@ -30,13 +45,24 @@ int main(){
             scanf("%c",&m);
             scanf("%c",&m);
             
-            printf("shanchuchenggong\n");
+            printf("shanchuchenggong,dianjihuichejixu\n");
             char x;
             scanf("%c",&x);
             scanf("%c",&x);
 
        }
        if( code == 3){
+             printf("suoyoumingganzifu\n");
+            for(int i=0;i<index;i++){
+                printf("%c\n",arr[i]);
+            }
+            char m;
+            scanf("%c",&m);
+            scanf("%c",&m);
+            printf("dianjihuichejixu\n");
+            char x;
+            scanf("%c",&x);
+            scanf("%c",&x);
 
        }
        if( code == 4){

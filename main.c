3c3323d02c35fa81d439d535623902f20b71da56
shadiao <1498800286@qq.com>
2019-01-17 15:25:35 +0800

    添加敏感字符

--- main.c
+++ main.c
@@ -2,8 +2,44 @@
 
 int main(){
 
-    printf("hello world!");
+   printf("jinrumingganzifuguanlixitong\n");
+     while(1){
+        printf("1---tianjiamingganzifu\n");
+        printf("2---chanchumingganzifu\n");
+        printf("3---suoyoumingganzifu\n");
+        printf("4---tihuanmingganzifu\n");
+        printf("5---tuichu\n");
+
+        printf("qingshuruxiangyinggongnengbianhao\n");
+        int code;
+        scanf("%d",&code);
+       if( code == 1){
+             printf("qingshuruyaotianjiademingganzifu\n");
+              char m;
+              scanf("%c",&m);
+              scanf("%c",&m);
+             
+              printf("tianjiachenggong\n");
+              char x;
+              scanf("%c",&x);
+              scanf("%c",&x);
+ }
+       if( code == 2){
+
+       }
+       if( code == 3){
+
+       }
+       if( code == 4){
+
+       }
+       if( code == 5){
+
+       }
+       
+              
+  
 
     return 0;
-
+     }
 }
